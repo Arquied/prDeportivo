e94a917a89abb48f327fdde611049617eee3b1b3
Arquied <arquied_88@hotmail.com>
2015-04-24 11:03:13 +0200

    Update main.php

--- aplicacion/vistas/plantillas/main.php
+++ aplicacion/vistas/plantillas/main.php
@@ -10,58 +10,56 @@
 				<title><?php echo $titulo;?></title>
 				<!-- definiciones comunes a todo el sitio -->
 				<link type="text/css" href="/estilos/bootstrap.min.css" rel="stylesheet" />
-				<link type="text/css" href="/estilos/carousel.css" rel="stylesheet" />
+				<!--<link type="text/css" href="/estilos/carousel.css" rel="stylesheet" />-->
 				<link type="text/css" href="/estilos/principal.css" rel="stylesheet"/>
 			</head>
-			<body>		
-		        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-		          <div class="container">
-		            <div class="navbar-header">
-		              <a class="navbar-brand" href="<?php Sistema::app()->generaURL(array("inicial","inicio")) ?>">Centro Deportivo</a>
-		            </div>
-		            <?php
-		            $nologeado = true;
-					$esAdmin=false;
-					$logeado=false;
-					if (Sistema::app()->Acceso()->hayUsuario()){
-						$nologeado=false;
-						$logeado=true;
-					}
-					else {
-						$nologeado=true;
-						$logeado=false;	
-					}
-					if(Sistema::app()->Acceso()->puedeConfigurar()){
-						$esAdmin=true;
-					}
-					else {
-						$esAdmin=false;	
-					}
-		            $quien = array("TEXTO"=>"Quienes Somos","URL"=>Sistema::app()->generaURL(array("inicial","quien")),"SUBMENU"=>false,"ACTIVO"=>true,"ITEMS"=>array());
-					$registra = array("TEXTO"=>"Registrate","URL"=>Sistema::app()->generaURL(array("usuarios","registro")),"SUBMENU"=>false,"ACTIVO"=>$nologeado,"ITEMS"=>array());
-		            $login = array("TEXTO"=>"Login","URL"=>Sistema::app()->generaURL(array("inicial","login")),"SUBMENU"=>false,"ACTIVO"=>$nologeado,"ITEMS"=>array());
-					$logout = array("TEXTO"=>"Logout","URL"=>Sistema::app()->generaURL(array("inicial","cerrarSesion")),"SUBMENU"=>false,"ACTIVO"=>$logeado,"ITEMS"=>array());
-					$perfil = array("TEXTO"=>"Mi Perfil","URL"=>Sistema::app()->generaURL(array()),"SUBMENU"=>false,"ACTIVO"=>$logeado,"ITEMS"=>array());
-					$servicios = array("TEXTO"=>"Servicios","URL"=>Sistema::app()->generaURL(array()),"SUBMENU"=>false,"ACTIVO"=>true,"ITEMS"=>array());
-					$instalaciones = array("TEXTO"=>"Instalaciones","URL"=>Sistema::app()->generaURL(array()),"SUBMENU"=>false,"ACTIVO"=>true,"ITEMS"=>array());
-					$usuarios = array("TEXTO"=>"Usuarios","URL"=>Sistema::app()->generaURL(array()),"SUBMENU"=>false,"ACTIVO","ITEMS"=>array());
-					$administrar = array("TEXTO"=>"Administrar","URL"=>"","SUBMENU"=>true,"ACTIVO"=>true,"ITEMS"=>array($instalaciones,$servicios,$usuarios));
-		            $datos = array($quien,$registra,$login,$logout,$perfil,$administrar);
-		            $cbarra = new CBarraMenu($datos);
-					$cbarra->dibujate();
-		           ?>
-		          </div>
-		        </nav>
-			
-			     
-				
-					<div id="conte" role="main" class="theme-showcase">
-						
-						<?php echo $contenido; 
-						
-						   
-						?>
-					</div>
+			<body>
+			    <div>	
+    		        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    		          <div>
+    		            <div class="navbar-header">
+    		              <a class="navbar-brand" href="<?php Sistema::app()->generaURL(array("inicial","inicio")) ?>">Centro Deportivo</a>
+    		            </div>
+    		            <?php
+    		            $nologeado = true;
+    					$esAdmin=false;
+    					$logeado=false;
+    					if (Sistema::app()->Acceso()->hayUsuario()){
+    						$nologeado=false;
+    						$logeado=true;
+    					}
+    					else {
+    						$nologeado=true;
+    						$logeado=false;	
+    					}
+    					if(Sistema::app()->Acceso()->puedeConfigurar()){
+    						$esAdmin=true;
+    					}
+    					else {
+    						$esAdmin=false;	
+    					}
+    		            $quien = array("TEXTO"=>"Quienes Somos","URL"=>Sistema::app()->generaURL(array("inicial","quien")),"SUBMENU"=>false,"ACTIVO"=>true,"ITEMS"=>array());
+    					$registra = array("TEXTO"=>"Registrate","URL"=>Sistema::app()->generaURL(array("usuarios","registro")),"SUBMENU"=>false,"ACTIVO"=>$nologeado,"ITEMS"=>array());
+    		            $login = array("TEXTO"=>"Login","URL"=>Sistema::app()->generaURL(array("inicial","login")),"SUBMENU"=>false,"ACTIVO"=>$nologeado,"ITEMS"=>array());
+    					$logout = array("TEXTO"=>"Logout","URL"=>Sistema::app()->generaURL(array("inicial","cerrarSesion")),"SUBMENU"=>false,"ACTIVO"=>$logeado,"ITEMS"=>array());
+    					$perfil = array("TEXTO"=>"Mi Perfil","URL"=>Sistema::app()->generaURL(array("usuarios", "miPerfil")),"SUBMENU"=>false,"ACTIVO"=>$logeado,"ITEMS"=>array());
+    					$servicios = array("TEXTO"=>"Servicios","URL"=>Sistema::app()->generaURL(array()),"SUBMENU"=>false,"ACTIVO"=>true,"ITEMS"=>array());
+    					$instalaciones = array("TEXTO"=>"Instalaciones","URL"=>Sistema::app()->generaURL(array()),"SUBMENU"=>false,"ACTIVO"=>true,"ITEMS"=>array());
+    					$usuarios = array("TEXTO"=>"Usuarios","URL"=>Sistema::app()->generaURL(array()),"SUBMENU"=>false,"ACTIVO","ITEMS"=>array());
+    					$administrar = array("TEXTO"=>"Administrar","URL"=>"","SUBMENU"=>true,"ACTIVO"=>true,"ITEMS"=>array($instalaciones,$servicios,$usuarios));
+    		            $datos = array($quien,$registra,$login,$logout,$perfil,$administrar);
+    		            $cbarra = new CBarraMenu($datos);
+    					$cbarra->dibujate();
+    		           ?>
+    		          </div>
+    		        </nav>				
+    				<div id="conte" role="main" class="theme-showcase">					
+    					<?php 
+    					   echo $contenido; 		
+    					?>
+    				</div>
+				</div>  
+					
 					<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 					<script src="script/bootstrap.min.js"></script>
 			</body>
