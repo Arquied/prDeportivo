f9f11ed8b7a7555ce08162e0c5732f382da73171
Arquied <arquied_88@hotmail.com>
2015-04-24 10:57:23 +0200

    Create listaActividades.php

---
+++ aplicacion/vistas/actividades/listaActividades.php
@@ -0,0 +1,61 @@
+<?php
+
+    //Obtener la lista de categorías para el filtrado
+    $categoria=new Categorias();
+    $listaCat=array();
+    foreach ($categoria->buscarTodos() as $cat) {
+        $listaCat[$cat["cod_categoria"]]=$cat["nombre"];
+    }
+    echo CHTML::cssFichero("/estilos/estiloActividades.css");
+    
+    //FILTRADO
+    echo CHTML::dibujaEtiqueta("div", array("class"=>"contFiltrado"));
+                echo CHTML::dibujaEtiqueta("div", array("class"=>"contTitFormulario"));
+                    echo CHTML::dibujaEtiqueta("h4", array(), "Campos de filtrado", true);                
+                echo CHTML::dibujaEtiquetaCierre("div");
+
+                //FORMULARIO DE FILTRADO
+                echo CHTML::iniciarForm("", "post", array("role"=>"form"));
+                    //Campo categoria           
+                    echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
+                        echo CHTML::campoLabel("Categorías", "categoria");                            
+                        echo CHTML::campoListaDropDown("categoria", "", $listaCat, array("class"=>"form-control"));
+                    echo CHTML::dibujaEtiquetaCierre("div");
+                    
+                    //Campo nombre actividad
+                    echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
+                        echo CHTML::campoLabel("Nombre", "nombre");
+                        echo CHTML::campoText("nombre", "", array("class"=>"form-control", "size"=>30));
+                    echo CHTML::dibujaEtiquetaCierre("div");
+                    
+                    //Boton submit
+                    echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
+                        echo CHTML::campoBotonSubmit("Filtrar", array("class"=>"btn btn-default"));                 
+                    echo CHTML::dibujaEtiquetaCierre("div");
+                
+                echo CHTML::finalizarForm();
+    echo CHTML::dibujaEtiquetaCierre("div");
+    
+    //ACTIVIDADES
+    echo CHTML::dibujaEtiqueta("div", array("class"=>"container marketing"));
+        if(count($filas)==0){
+            echo CHTML::dibujaEtiqueta("div");
+                echo CHTML::dibujaEtiqueta("h2", array(), "No existe resultado de busqueda", true);
+            echo CHTML::dibujaEtiquetaCierre("div");
+        }
+        else{
+            foreach ($filas as $clave => $valor) {
+                $this->dibujaVistaParcial("listaActividadesParcial", array("fila"=>$valor, "clave"=>$clave));
+            }    
+        }
+        
+    
+    echo CHTML::dibujaEtiquetaCierre("div");
+    
+    
+    //PAGINADOR
+    echo CPager::requisitos();
+    if(count($filas)>0){
+        $pagi=new CPager($paginador,array());
+        echo $pagi->dibujate();
+    }
