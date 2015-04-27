216ccbe8c870673bbe3ba4a5c114d9aecfad4a19
axisco91 <darthxisco@gmail.com>
2015-04-27 22:56:19 +0200

    Create privacidad.php

---
+++ aplicacion/vistas/inicial/privacidad.php
@@ -0,0 +1,28 @@
+<?php
+
+echo CHTML::dibujaEtiqueta("div",array("class"=>"container"));
+
+echo CHTML::dibujaEtiqueta("h3",array(),"CONDICIONES GENERALES DE USO Y POLÍTICA DE PROTECCIÓN DE DATOS DE CARÁCTER PERSONAL",true);
+echo CHTML::dibujaEtiqueta("p",array(),"Por favor, lea atentamente estas condiciones de uso antes de utilizar este sitio Web.
+	La utilización de este sitio Web supone que el usuario reconoce implícitamente que acepta las condiciones.
+	Si no estás de acuerdo con las mismas o tienes alguna duda te rogamos que no te registres ni utilices este sitio Web.",true);
+
+echo CHTML::dibujaEtiqueta("h3", array(),"POLÍTICA DE PROTECCIÓN DE DATOS",true);
+echo CHTML::dibujaEtiqueta("p",array(),"La página Web centroDeportivo.es tiene como propósito principal informar y facilitar información a los ciudadanos de nuestro municipio.",true);
+echo CHTML::dibujaEtiqueta("p",array(),"centroDeportivo asume las obligaciones legales en materia de protección de datos de carácter personal, especialmente en lo referido a:",true);
+echo CHTML::dibujaEtiqueta("p",array(),"1. La existencia de ficheros o tratamientos con datos de carácter personal.",true);
+echo CHTML::dibujaEtiqueta("p",array(),"2. La finalidad de la recogida de datos.",true);
+echo CHTML::dibujaEtiqueta("p",array(),"3. Los destinatarios de la información.",true);
+echo CHTML::dibujaEtiqueta("p",array(),"4. El carácter facultativo u obligatorio de las preguntas que hagamos.",true);
+echo CHTML::dibujaEtiqueta("p",array(),"5. Las consecuencias de la obtención de los datos o de la negativa a suministrarlos.",true);
+echo CHTML::dibujaEtiqueta("p",array(),"6. La posibilidad de ejercitar los derechos de acceso, rectificación, cancelación y oposición.",true);
+echo CHTML::dibujaEtiqueta("p",array(),"7. La identidad y dirección del responsable del tratamiento o en su caso de su representante.",true);
+
+echo CHTML::dibujaEtiqueta("h3", array(),"ACEPTACIÓN DE LAS PRESENTES CONDICIONES",true);
+echo CHTML::dibujaEtiqueta("p",array(),"Al utilizar este Web, asumimos que el usuario ha leído este documento cuidadosamente y que ha entendido y seguido sus consejos.
+	En caso de que el usuario no esté de acuerdo con esta política de protección de datos o con los consejos que a través de la misma se dan,
+	por favor no utilices nuestra página web. Nos reservamos el derecho a modificar la presente condición de uso cuando consideremos que es necesario.
+	La continuidad por el usuario en el uso de este y de otros Web de centroDeportivo, después de haber introducido dichos cambios
+	implicará la total aceptación de los mismos.",true);
+	
+echo CHTML::dibujaEtiquetaCierre("div");
