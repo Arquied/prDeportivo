18c7c7bec6d7a2f007bbe8331d6d08af589f1619
Arquied <arquied_88@hotmail.com>
2015-05-01 12:25:25 +0200

    Update Configuracion.php

--- aplicacion/modelos/Configuracion.php
+++ aplicacion/modelos/Configuracion.php
@@ -18,14 +18,16 @@
         }
             
         protected function fijarAtributos(){
-            return array("cod_configuracion", "nombre_empresa", "cif", "logo");
+            return array("cod_configuracion", "nombre_empresa", "cif", "logo", "url_facebook", "url_twitter");
         }
         
         protected function fijarDescripciones(){
             return array("cod_configuracion"=>"Código de configuración",
                         "nombre_empresa"=>"Nombre de la empresa",
                         "cif"=>"Cif empresa",
-                        "logo"=>"Logo de la empresa"
+                        "logo"=>"Logo de la empresa",
+                        "url_facebook"=>"URL facebook",
+                        "url_twitter"=>"URL twitter"
                         );
         }
         
@@ -34,7 +36,9 @@
                         array("ATRI"=>"cod_configuracion", "TIPO"=>"ENTERO", "MIN"=>0, "MENSAJE"=>"El código de la configuración debe ser positivo", "DEFECTO"=>0),
                         array("ATRI"=>"nombre_empresa", "TIPO"=>"CADENA", "TAMANIO"=>50, "MENSAJE"=>"El nombre de empresa no puede ser tan largo", "DEFECTO"=>""),
                         array("ATRI"=>"cif", "TIPO"=>"CADENA", "TAMANIO"=>9, "MENSAJE"=>"El cif no puede ser tan largo", "DEFECTO"=>""),
-                        array("ATRI"=>"logo", "TIPO"=>"CADENA", "TAMANIO"=>100, "MENSAJE"=>"El logo no puede ser tan largo", "DEFECTO"=>"")
+                        array("ATRI"=>"logo", "TIPO"=>"CADENA", "TAMANIO"=>100, "MENSAJE"=>"El logo no puede ser tan largo", "DEFECTO"=>""),
+                        array("ATRI"=>"url_facebook", "TIPO"=>"CADENA", "TAMANIO"=>100, "MENSAJE"=>"La url de facebook no puede ser tan larga", "DEFECTO"=>""),
+                        array("ATRI"=>"url_twitter", "TIPO"=>"CADENA", "TAMANIO"=>100, "MENSAJE"=>"La url de twitter no puede ser tan larga", "DEFECTO"=>""),
                         );
         }
         
@@ -43,6 +47,8 @@
            $this->nombre_empresa="";
            $this->cif="";
            $this->logo="";
+           $this->url_facebook="";
+		   $this->url_twitter="";
               
         }   
         
@@ -53,12 +59,14 @@
         protected function fijarSentenciaInsert(){
             $nombre_empresa=CGeneral::addSlashes($this->nombre_empresa);
             $cif=CGeneral::addSlashes($this->cif);
-            $logo=CGeneral::addSlashes($this->logo);            
+            $logo=CGeneral::addSlashes($this->logo);        
+            $url_facebook=CGeneral::addSlashes($this->url_facebook);
+			$url_twitter=CGeneral::addSlashes($this->url_twitter);    
                                 
             return "insert into configuracion (".
-                        " nombre_empresa, cif, logo ".
+                        " nombre_empresa, cif, logo, url_facebook, url_twitter ".
                         " ) values ( ".
-                        " '$nombre_empresa', '$cif', '$logo' ".
+                        " '$nombre_empresa', '$cif', '$logo', '$url_facebook', '$url_twitter' ".
                         " ) " ;
         }
         
@@ -66,11 +74,15 @@
             $nombre_empresa=CGeneral::addSlashes($this->nombre_empresa);
             $cif=CGeneral::addSlashes($this->cif);
             $logo=CGeneral::addSlashes($this->logo);
+			$url_facebook=CGeneral::addSlashes($this->url_facebook);
+			$url_twitter=CGeneral::addSlashes($this->url_twitter);
                 
             return "update configuracion set ".
                             " nombre_empresa='$nombre_empresa', ".
                             " cif='$cif', ".
-                            " logo='$logo' ".
+                            " logo='$logo', ".
+                            " url_facebook='$url_facebook', ".
+                            " url_twitter='$url_twitter' ".
                             " where cod_configuracion={$this->cod_configuracion} ";                                                                       
         }           
         
