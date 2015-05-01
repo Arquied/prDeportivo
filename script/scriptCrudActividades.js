0b28b8d5f4f4ed62ad85b4f31a3ce67fdaa20cfc
Arquied <arquied_88@hotmail.com>
2015-05-01 12:35:21 +0200

    Create scriptCrudActividades.js

---
+++ script/scriptCrudActividades.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	//EVENTO ONCLICK PARA EL BOTON BORRAR, MUESTRA VENTANA MODAL ESTA SEGURO
+	$(".btnBorrar").on("click", function(){
+		$('#modalBorrado').modal({show:true});
+		var id=$(this).parent().siblings().first().html()
+		$("#modalBorrado").data('id_actividad', id);		
+	});
+	//EVENTO ONCLICK CONFIRMAR BORRADO REDIRECCIONA PASANDOLE EL ID_ACTIVIDAD
+	$("#seguroBorrar").on("click", function(){
+		window.location="index.php?co=actividades&ac=borraActividad&id="+$("#modalBorrado").data("id_actividad");
+	});
+});
