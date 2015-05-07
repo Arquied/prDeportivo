4ccd0310491d2a994e016bfa4f2253ba4ef7dc46
axisco91 <darthxisco@gmail.com>
2015-05-07 21:48:42 +0200

    Create modificaHorario.php

---
+++ aplicacion/vistas/horarios/modificaHorario.php
@@ -0,0 +1,80 @@
+<?php
+
+// OBTENER ERRORES
+$errores=$modelo->getErrores();
+
+echo CHTML::cssFichero("/estilos/estiloFormularios.css");
+
+    echo CHTML::dibujaEtiqueta("div", array("class"=>"container contForm"));
+                echo CHTML::dibujaEtiqueta("div", array("class"=>"contTitFormulario"));
+                    echo CHTML::dibujaEtiqueta("h2", array(), "Modificar horario", true);                
+                echo CHTML::dibujaEtiquetaCierre("div");
+
+				// FORMULARIO DE MODIFICAR HORARIO
+                echo CHTML::iniciarForm("", "post", array("role"=>"form", "enctype"=>"multipart/form-data"));
+                
+                    echo CHTML::dibujaEtiqueta("div");
+                              
+                        //Campo temporada               
+                        echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
+                            if(isset($errores["cod_temporada"])){
+                                echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["cod_temporada"], true);
+                            }
+                            echo CHTML::campoLabel("Temporadas", "temporada");                            
+                            echo CHTML::campoListaDropDown("temporada", "", Temporadas::listaTemporadas(), array("class"=>"form-control"));
+                        echo CHTML::dibujaEtiquetaCierre("div");                  
+                        
+                        // Campo dia
+                        echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
+							if (isset($errores["dia"])){
+								echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["dia"], true);
+                            }
+							echo CHTML::campoListaCheckBox("dia", "", Dias::listaDias());
+					    echo CHTML::dibujaEtiquetaCierre("div");
+						
+                        //Campo hora_inicio
+                        echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
+                            if(isset($errores["hora_inicio"])){
+                                echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["hora_inicio"], true);
+                            }
+                        echo CHTML::modeloLabel($modelo, "hora_inicio");
+                            echo CHTML::modeloText($modelo, "hora_inicio", array("class"=>"form-control", "maxlength"=>10, "size"=>10, "placeholder"=>"hh:mm:ss"));                        
+                        echo CHTML::dibujaEtiquetaCierre("div"); 
+						
+                        //Campo hora_fin
+                        echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
+                            if(isset($errores["hora_fin"])){
+                                echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["hora_fin"], true);
+                            }
+                        echo CHTML::modeloLabel($modelo, "hora_fin");
+                            echo CHTML::modeloText($modelo, "hora_fin", array("class"=>"form-control", "maxlength"=>10, "size"=>10, "placeholder"=>"hh:mm:ss"));                        
+                        echo CHTML::dibujaEtiquetaCierre("div"); 
+						
+                        //Campo fecha_inicio
+                        echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
+                            if(isset($errores["fecha_inicio"])){
+                                echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["fecha_inicio"], true);
+                            }
+                            echo CHTML::modeloLabel($modelo, "fecha_inicio");
+                            echo CHTML::modeloText($modelo, "fecha_inicio", array("class"=>"form-control", "maxlength"=>10, "size"=>10, "placeholder"=>"dd/mm/yyyy"));                        
+                        echo CHTML::dibujaEtiquetaCierre("div");  
+                        
+                        //Campo fecha_fin
+                        echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
+                            if(isset($errores["fecha_fin"])){
+                                echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["fecha_fin"], true);
+                            }
+                            echo CHTML::modeloLabel($modelo, "fecha_fin");
+                            echo CHTML::modeloText($modelo, "fecha_fin", array("class"=>"form-control", "maxlength"=>10, "size"=>10, "placeholder"=>"dd/mm/yyyy"));                        
+                        echo CHTML::dibujaEtiquetaCierre("div"); 
+
+                        //Boton insertar
+                        echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
+                            echo CHTML::campoBotonSubmit("Modificar", array("class"=>"btn btn-default"));                 
+                        echo CHTML::dibujaEtiquetaCierre("div");
+                    echo CHTML::dibujaEtiquetaCierre("div");
+                    
+                echo CHTML::finalizarForm();
+                
+    echo CHTML::dibujaEtiquetaCierre("div");
+                
