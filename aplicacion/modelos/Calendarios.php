75dbe2ebfcb9fa9af5b729744ba0db19e97d8145
axisco91 <darthxisco@gmail.com>
2015-05-07 21:40:55 +0200

    Update Calendarios.php

--- aplicacion/modelos/Calendarios.php
+++ aplicacion/modelos/Calendarios.php
@@ -18,17 +18,18 @@
 		}
 			
 		protected function fijarAtributos(){
-			return array("cod_calendario", "cod_dia", "cod_actividad", "hora_inicio", "hora_fin", "fecha_inicio", "fecha_fin");
+			return array("cod_calendario", "cod_dia", "cod_actividad", "hora_inicio", "hora_fin", "fecha_inicio", "fecha_fin", "disponible");
 		}
 		
 		protected function fijarDescripciones(){
 			return array("cod_calendario"=>"Código calendario actividad",
 						"cod_dia"=>"Código dia semana",
 						"cod_actividad"=>"Código de la actividad",
-						"hora_inicio"=>"Hora de inicio",
-						"hora_fin"=>"Hora de fin",
-						"fecha_inicio"=>"Fecha inicio de la actividad",
-						"fecha_fin"=>"Fecha fin de la actividad"
+                            "hora_inicio"=>"Hora de inicio:",
+                            "hora_fin"=>"Hora de finalización:",
+                            "fecha_inicio"=>"Fecha de inicio:",
+                            "fecha_fin"=>"Fecha de fin:",
+						"disponible"=>"Está disponible"
 						);
 		}
 		
@@ -42,7 +43,12 @@
 						array("ATRI"=>"hora_inicio", "TIPO"=>"HORA"),
 						array("ATRI"=>"hora_fin", "TIPO"=>"HORA"),
 						array("ATRI"=>"fecha_inicio", "TIPO"=>"FECHA"),
-						array("ATRI"=>"fecha_fin", "TIPO"=>"FECHA")
+                         array("ATRI"=>"fecha_inicio", "TIPO"=>"REQUERIDO"),
+                         array("ATRI"=>"fecha_inicio","TIPO"=>"FUNCTION","FUNCTION"=>"validaFechaInicio"),
+						array("ATRI"=>"fecha_fin", "TIPO"=>"FECHA"),
+                         array("ATRI"=>"fecha_fin","TIPO"=>"REQUERIDO"),
+                         array("ATRI"=>"fecha_fin","TIPO"=>"FUNCTION", "FUNCTION"=>"validaFechaFin"),
+						array("ATRI"=>"disponible", "TIPO"=>"ENTERO", "MIN"=>0, "MAX"=>1)
 						);
 		}
 		
@@ -50,11 +56,18 @@
 			$this->cod_calendario=1;
 			$this->cod_actividad=0;
 			$this->cod_dia=0;
-			$this->hora_inicio="00:00:00";
-			$this->hora_fin="00:00:00";
-			$this->fecha_inicio=date("d/m/Y");
-			$this->fecha_fin=date("d/m/Y");
+
+            $fech = new DateTime();   
+			         
+            $hora = $fech->format("H:i:s");
+            $this->hora_inicio = $hora;
+            $this->hora_fin = $hora;
+            
+            $fecha = $fech->format("d/m/Y");
+            $this -> fecha_inicio = $fecha;
+            $this -> fecha_fin = $fecha;    
 			
+			$this->disponible=1;
 		}	
 		
 		protected function afterBuscar(){
@@ -67,28 +80,48 @@
 			$this->fecha_fin=$fecha;
 		}	
 		
+        protected function validaFechaInicio(){        
+            $fecha1=DateTime::createFromFormat('d/m/Y',$this->fecha_inicio);
+            
+            $fecha2 = new DateTime();
+            $fecha2= $fecha2->format("d/m/Y");
+            if ($fecha1<$fecha2){
+                $this->setError("fecha_inicio","La fecha de inicio debe ser posterior a ".$fecha2);
+            }
+        }
+        
+        protected function validaFechaFin(){        
+            $fecha1=DateTime::createFromFormat('d/m/Y',$this->fecha_fin);
+            $fecha2=DateTime::createFromFormat('d/m/Y',$this->fecha_inicio);
+            if ($fecha1<$fecha2){
+                $this->setError("fecha_fin","La fecha de finalización debe ser posterior a ".$fecha2);
+            }
+        }
+		
 		protected function fijarSentenciaInsert(){
 			$cod_actividad=$this->cod_actividad;
 			$cod_dia=$this->cod_dia;
-			$hora_inicio=CGeneral::addSlashes($this->hora_inicio);
-			$hora_fin=CGeneral::addSlashes($this->hora_fin);
-			$fecha_inicio=CGeneral::fechaNormalAMysql($this->fecha_inicio);
-			$fecha_fin=CGeneral::fechaNormalAMysql($this->fecha_fin);
-								
+            $fecha_inicio=CGeneral::fechaNormalAMysql($this->fecha_inicio);
+            $fecha_fin=CGeneral::fechaNormalAMysql($this->fecha_fin);
+            $hora_inicio=CGeneral::addSlashes($this->hora_inicio);
+            $hora_fin=CGeneral::addSlashes($this->hora_fin);
+            
+			$disponible=intval($this->disponible);					
 			return "insert into calendarios (".
-						" cod_actividad, cod_dia, hora_inicio, hora_fin, fecha_inicio, fecha_fin ".
+						" cod_actividad, cod_dia, hora_inicio, hora_fin, fecha_inicio, fecha_fin, disponible ".
 						" ) values ( ".
-						" $cod_actividad, $cod_dia, '$hora_inicio', '$hora_fin', '$fecha_inicio', '$fecha_fin' ".
+						" $cod_actividad, $cod_dia, '$hora_inicio', '$hora_fin', '$fecha_inicio', '$fecha_fin', $disponible ".
 						" ) " ;
 		}
 		
 		protected function fijarSentenciaUpdate(){
 			$cod_actividad=$this->cod_actividad;
 			$cod_dia=$this->cod_dia;
-			$hora_inicio=CGeneral::addSlashes($this->hora_inicio);
-			$hora_fin=CGeneral::addSlashes($this->hora_fin);
-			$fecha_inicio=CGeneral::fechaNormalAMysql($this->fecha_inicio);
+            $fecha_inicio=CGeneral::fechaNormalAMysql($this->fecha_inicio);
             $fecha_fin=CGeneral::fechaNormalAMysql($this->fecha_fin);
+            $hora_inicio=CGeneral::addSlashes($this->hora_inicio);
+            $hora_fin=CGeneral::addSlashes($this->hora_fin);
+			$disponible=intval($this->disponible);
 			
 			return "update calendario_actividad set ".
 							" cod_actividad=$cod_actividad, ".
@@ -96,7 +129,8 @@
 							" hora_inicio='$hora_inicio', ".
 							" hora_fin='$hora_fin', ".
 							" fecha_inicio='$fecha_inicio', ".
-							" fecha_fin='$fecha_fin' ".
+							" fecha_fin='$fecha_fin', ".
+							" disponible=$disponible ".
 							" where cod_calendario={$this->cod_calendario} ";																		
 		}
 		
