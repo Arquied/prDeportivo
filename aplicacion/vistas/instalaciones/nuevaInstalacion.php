3dafa6d867fe4dc6e2b2f1bbd66bde15832916ae
axisco91 <darthxisco@gmail.com>
2015-05-07 21:30:27 +0200

    Update nuevaInstalacion.php

--- aplicacion/vistas/instalaciones/nuevaInstalacion.php
+++ aplicacion/vistas/instalaciones/nuevaInstalacion.php
@@ -1,4 +1,6 @@
 <?php
+
+// OBTENER ERRORES
 $errores = $modelo->getErrores();
 
 echo CHTML::cssFichero("/estilos/estiloFormularios.css");
@@ -8,9 +10,11 @@ echo CHTML::dibujaEtiqueta("div", array("class"=>"contTitFormulario"));
 echo CHTML::dibujaEtiqueta("h2", array(), "Instalacion Nueva", true);                
 echo CHTML::dibujaEtiquetaCierre("div");
 
+ //FORMULARIO DE NUEVA INSTALACION
 echo CHTML::iniciarForm("#","POST",array("role"=>"form"));
 echo CHTML::dibujaEtiqueta("div", array("class"=>"col-sm-offset-3 col-sm-6"));
 
+// Campo nombre
 echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
 if (isset($errores["nombre"])){
 	echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["nombre"],true);
@@ -19,6 +23,7 @@ echo CHTML::modeloLabel($modelo, "nombre");
 echo CHTML::modeloText($modelo, "nombre",array("class"=>"form-control","maxlength"=>50,"size"=>51));
 echo CHTML::dibujaEtiquetaCierre("div");
 
+// Campo descripcion
 echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
 if (isset($errores["descripcion"])){
 	echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["descripcion"],true);
@@ -27,6 +32,7 @@ echo CHTML::modeloLabel($modelo, "descripcion");
 echo CHTML::modeloTextArea($modelo, "descripcion",array("class"=>"form-control",));
 echo CHTML::dibujaEtiquetaCierre("div");
 
+// Campo imagen
 echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
 if (isset($errores["imagen"])){
 	echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["imagen"],true);
@@ -35,6 +41,7 @@ echo CHTML::modeloLabel($modelo, "imagen");
 echo CHTML::modeloFile($modelo, "imagen");
 echo CHTML::dibujaEtiquetaCierre("div");
 
+// Campo capacidaad
 echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
 if (isset($errores["capacidad"])){
 	echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["capacidad"],true);
@@ -43,6 +50,7 @@ echo CHTML::modeloLabel($modelo, "capacidad");
 echo CHTML::modeloNumber($modelo, "capacidad",array("class"=>"form-control",));
 echo CHTML::dibujaEtiquetaCierre("div");
 
+// Boton insertar
 echo CHTML::dibujaEtiqueta("div");
 echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
 echo CHTML::campoBotonSubmit("Nueva", array("class"=>"btn btn-default"));
