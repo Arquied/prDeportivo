b677dd2c5d955357cbeee84c6079e84b5a57a9d7
axisco91 <darthxisco@gmail.com>
2015-05-10 21:54:12 +0200

    Create scriptCurdCalendarios.js

---
+++ script/scriptCurdCalendarios.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	//EVENTO ONCLICK PARA EL BOTON BORRAR, MUESTRA VENTANA MODAL ESTA SEGURO
+	$(".btnBorrar").on("click", function(){
+		$('#modalBorrado').modal({show:true});
+		var id=$(this).parent().siblings().first().html()
+		$("#modalBorrado").data('id_calendario', id);		
+	});
+	//EVENTO ONCLICK CONFIRMAR BORRADO REDIRECCIONA PASANDOLE EL ID_CALENDARIOS
+	$("#seguroBorrar").on("click", function(){
+		window.location="index.php?co=temporadas&ac=borraTemporada&id="+$("#modalBorrado").data("id_calendario");
+	});
+});
