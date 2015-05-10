0d2cd06ef67ab0839ed2080a1cec2d7255307cb0
axisco91 <darthxisco@gmail.com>
2015-05-10 22:05:29 +0200

    Update indexCalendario.php

--- aplicacion/vistas/calendarios/indexCalendario.php
+++ aplicacion/vistas/calendarios/indexCalendario.php
@@ -11,7 +11,7 @@
     echo CHTML::dibujaEtiquetaCierre("div");
 	
 	echo CHTML::dibujaEtiqueta("div");
-	echo CHTML::dibujaEtiqueta("a",array("href"=>Sistema::app()->generaURL(array("calendarios", "nuevoCalendario"))),"Nuevo");
+	echo CHTML::dibujaEtiqueta("a",array("href"=>Sistema::app()->generaURL(array("calendarios", "nuevoCalendario")), "class" => "btn btn-default"),"Nuevo");
 	echo CHTML::dibujaEtiquetaCierre("div");
 	
     echo CHTML::dibujaEtiqueta("div", array("id"=>"contActividades"));
