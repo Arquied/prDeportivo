b526d7d90943bde886591e9b55d415dc51867b3d
axisco91 <darthxisco@gmail.com>
2015-05-17 22:38:57 +0200

    Update nuevaTemporada.php

--- aplicacion/vistas/temporadas/nuevaTemporada.php
+++ aplicacion/vistas/temporadas/nuevaTemporada.php
@@ -3,7 +3,11 @@
 // OBTENER ERRORES
 $errores = $modelo -> getErrores();
 
-echo CHTML::cssFichero("/estilos/estiloFormularios.css");
+	echo CHTML::cssFichero("/estilos/estiloFormularios.css");
+	echo CHTML::cssFichero("../../estilos/jquery.datetimepicker.css");
+	echo CHTML::scriptFichero("../../script/jquery.js");
+	echo CHTML::scriptFichero("../../script/jquery.datetimepicker.js");
+	echo CHTML::scriptFichero("../../script/scriptFecha.js");
 
 echo CHTML::dibujaEtiqueta("div", array("class"=>"container contForm"));
 echo CHTML::dibujaEtiqueta("div", array("class"=>"contTitFormulario"));
@@ -25,12 +29,13 @@ echo CHTML::modeloText($modelo, "nombre", array("maxlength"=>50, "size"=>51));
 echo CHTML::dibujaEtiquetaCierre("div");
 
 // Campo fecha_inicio
+echo CHTML::dibujaEtiqueta("div");
 echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
 if (isset($errores["fecha_inicio"])){
 	echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["fecha_inicio"], true);
 }
 echo CHTML::modeloLabel($modelo, "fecha_inicio");
-echo CHTML::modeloDate($modelo, "fecha_inicio");
+echo CHTML::modeloText($modelo, "fecha_inicio", array("class"=>"form-control", "id"=>"fecha_inicio", "maxlength"=>10, "size"=>10, "placeholder"=>"dd/mm/yyyy")); 
 echo CHTML::dibujaEtiquetaCierre("div");
 
 // Campo fecha_fin
@@ -39,7 +44,8 @@ if (isset($errores["fecha_fin"])){
 	echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["fecha_fin"], true);
 }
 echo CHTML::modeloLabel($modelo, "fecha_fin");
-echo CHTML::modeloDate($modelo, "fecha_fin");
+ echo CHTML::modeloText($modelo, "fecha_fin", array("class"=>"form-control", "id"=>"fecha_fin", "maxlength"=>10, "size"=>10, "placeholder"=>"dd/mm/yyyy")); 
+echo CHTML::dibujaEtiquetaCierre("div");
 echo CHTML::dibujaEtiquetaCierre("div");
 
 // Boton insertar
