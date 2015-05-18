e2b864067c38c1f6aa21c8670bc3c3f2bab3d202
axisco91 <darthxisco@gmail.com>
2015-05-18 22:11:24 +0200

    Update Configuracion.php

--- aplicacion/modelos/Configuracion.php
+++ aplicacion/modelos/Configuracion.php
@@ -18,16 +18,19 @@
         }
             
         protected function fijarAtributos(){
-            return array("cod_configuracion", "nombre_empresa", "cif", "logo", "url_facebook", "url_twitter");
+            return array("cod_configuracion", "nombre_empresa", "cif", "direccion", "logo", "url_facebook", "url_twitter", "correo", "telefono");
         }
         
         protected function fijarDescripciones(){
             return array("cod_configuracion"=>"Código de configuración",
                         "nombre_empresa"=>"Nombre de la empresa",
                         "cif"=>"Cif empresa",
+                        "direccion"=>"Dirección",
                         "logo"=>"Logo de la empresa",
                         "url_facebook"=>"URL facebook",
-                        "url_twitter"=>"URL twitter"
+                        "url_twitter"=>"URL twitter",
+                        "correo"=> "Dirección de correo",
+                        "telefono"=> "Telefono"
                         );
         }
         
@@ -36,9 +39,12 @@
                         array("ATRI"=>"cod_configuracion", "TIPO"=>"ENTERO", "MIN"=>0, "MENSAJE"=>"El código de la configuración debe ser positivo", "DEFECTO"=>0),
                         array("ATRI"=>"nombre_empresa", "TIPO"=>"CADENA", "TAMANIO"=>50, "MENSAJE"=>"El nombre de empresa no puede ser tan largo", "DEFECTO"=>""),
                         array("ATRI"=>"cif", "TIPO"=>"CADENA", "TAMANIO"=>9, "MENSAJE"=>"El cif no puede ser tan largo", "DEFECTO"=>""),
+                        array("ATRI"=>"direccion", "TIPO"=>"CADENA", "TAMANIO"=>50, "MENSAJE"=>"El direccion no puede ser tan largo", "DEFECTO"=>""),
                         array("ATRI"=>"logo", "TIPO"=>"CADENA", "TAMANIO"=>100, "MENSAJE"=>"El logo no puede ser tan largo", "DEFECTO"=>""),
                         array("ATRI"=>"url_facebook", "TIPO"=>"CADENA", "TAMANIO"=>100, "MENSAJE"=>"La url de facebook no puede ser tan larga", "DEFECTO"=>""),
                         array("ATRI"=>"url_twitter", "TIPO"=>"CADENA", "TAMANIO"=>100, "MENSAJE"=>"La url de twitter no puede ser tan larga", "DEFECTO"=>""),
+						array("ATRI"=>"correo", "TIPO"=>"CADENA", "TAMANIO"=>30, "MENSAJE"=>"El email no puede ser tan largo"),
+						array("ATRI"=>"telefono", "TIPO"=>"CADENA", "TAMANIO"=>9, "MENSAJE"=>"El teléfono no puede ser tan largo")
                         );
         }
         
@@ -46,9 +52,12 @@
            $this->cod_configuracion=1;
            $this->nombre_empresa="";
            $this->cif="";
+		   $this->direccion="";
            $this->logo="";
            $this->url_facebook="";
 		   $this->url_twitter="";
+		   $this->coreo="";
+		   $this->telefono="";
               
         }   
         
@@ -59,30 +68,39 @@
         protected function fijarSentenciaInsert(){
             $nombre_empresa=CGeneral::addSlashes($this->nombre_empresa);
             $cif=CGeneral::addSlashes($this->cif);
+			$direccion=CGeneral::addSlashes($this->direccion);
             $logo=CGeneral::addSlashes($this->logo);        
             $url_facebook=CGeneral::addSlashes($this->url_facebook);
-			$url_twitter=CGeneral::addSlashes($this->url_twitter);    
+			$url_twitter=CGeneral::addSlashes($this->url_twitter);
+			$correo=CGeneral::addSlashes($this->correo);
+			$telefono=CGeneral::addSlashes($this->telefono);
                                 
             return "insert into configuracion (".
-                        " nombre_empresa, cif, logo, url_facebook, url_twitter ".
+                        " nombre_empresa, cif, direccion, logo, url_facebook, url_twitter, correo, telefono ".
                         " ) values ( ".
-                        " '$nombre_empresa', '$cif', '$logo', '$url_facebook', '$url_twitter' ".
+                        " '$nombre_empresa', '$cif', '$direccion', '$logo', '$url_facebook', '$url_twitter', '$correo', $telefono ".
                         " ) " ;
         }
         
         protected function fijarSentenciaUpdate(){
             $nombre_empresa=CGeneral::addSlashes($this->nombre_empresa);
             $cif=CGeneral::addSlashes($this->cif);
+			$direccion=CGeneral::addSlashes($this->direccion);
             $logo=CGeneral::addSlashes($this->logo);
 			$url_facebook=CGeneral::addSlashes($this->url_facebook);
 			$url_twitter=CGeneral::addSlashes($this->url_twitter);
+			$correo=CGeneral::addSlashes($this->correo);
+			$telefono=CGeneral::addSlashes($this->telefono);
                 
             return "update configuracion set ".
                             " nombre_empresa='$nombre_empresa', ".
                             " cif='$cif', ".
+                            " direccion='$direccion', ".
                             " logo='$logo', ".
                             " url_facebook='$url_facebook', ".
-                            " url_twitter='$url_twitter' ".
+                            " url_twitter='$url_twitter', ".
+                            " correo='$correo', ".
+                            " telefono='$telefono'".
                             " where cod_configuracion={$this->cod_configuracion} ";                                                                       
         }           
         
