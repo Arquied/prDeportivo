46363daebd861a2d2322e30020e87fcaa29bc4bd
axisco91 <darthxisco@gmail.com>
2015-05-24 11:47:34 +0200

    Create listaCompras.php

---
+++ aplicacion/vistas/compras/listaCompras.php
@@ -0,0 +1,98 @@
+<?php
+
+    echo CHTML::scriptFichero("../../script/jquery.dynatable.js");
+	echo CHTML::scriptFichero("../../script/scriptCrudCompras.js");
+    echo CHTML::cssFichero("../../estilos/jquery.dynatable.css");
+    echo CHTML::cssFichero("../../estilos/estiloActividades.css");
+    
+    //Titulo
+    echo CHTML::dibujaEtiqueta("div", array("class"=>"contTitulo"));
+        echo CHTML::dibujaEtiqueta("H1", array("class"=>"text-center"), "ADMINISTRACIÓN DE COMPRAS", TRUE);
+    echo CHTML::dibujaEtiquetaCierre("div");
+	
+    echo CHTML::dibujaEtiqueta("div", array("id"=>"contActividades"));
+        echo CHTML::dibujaEtiqueta("table", array("class"=>"table table-striped", "id"=>"tActividades"));
+            //DIBUJAR CABECERA DE LA TABLA
+            echo CHTML::dibujaEtiqueta("thead");
+                echo CHTML::dibujaEtiqueta("tr");
+                    echo CHTML::dibujaEtiqueta("th", array(), "CÓDIGO", TRUE);
+                    echo CHTML::dibujaEtiqueta("th", array(), "ACTIVIDAD", TRUE);
+                    echo CHTML::dibujaEtiqueta("th", array(), "IMPORTE_PAGADO", TRUE);
+					echo CHTML::dibujaEtiqueta("th", array(), "PENDIENTE", TRUE);
+                    echo CHTML::dibujaEtiqueta("th", array(), "FECHA COMPRA", TRUE);
+                    echo CHTML::dibujaEtiqueta("th", array(), "FECHA INICIO", TRUE);
+                    echo CHTML::dibujaEtiqueta("th", array(), "FECHA FIN", TRUE);
+					echo CHTML::dibujaEtiqueta("th", array(), "IMPORTE", TRUE);
+                    echo CHTML::dibujaEtiqueta("th", array(), "ANULADO", TRUE);
+                    echo CHTML::dibujaEtiqueta("th", array(), "OPCIONES", TRUE);
+                echo CHTML::dibujaEtiquetaCierre("tr");
+            echo CHTML::dibujaEtiquetaCierre("thead");
+            
+            //DIBUJAR CUERPO DE LA TABLA
+            echo CHTML::dibujaEtiqueta("tbody");
+                foreach ($filas as $fila) {
+                    echo CHTML::dibujaEtiqueta("tr");
+                        echo CHTML::dibujaEtiqueta("td", array(), $fila["cod_compra"], true);
+                        echo CHTML::dibujaEtiqueta("td", array(), Actividades::listaActividades($fila["cod_actividad"]), true);
+						echo CHTML::dibujaEtiqueta("td", array(), $fila["importe_pagado"], true);
+						if ($fila["pendiente"]){
+							echo CHTML::dibujaEtiqueta("td", array(), "NO PAGADO", true);
+						}
+						else {
+							echo CHTML::dibujaEtiqueta("td", array(), "PAGADO", true);
+						}
+						echo CHTML::dibujaEtiqueta("td", array(), $fila["fecha_compra"], true);
+                        echo CHTML::dibujaEtiqueta("td", array(), CGeneral::fechaMysqlANormal($fila["fecha_inicio"]), true);
+                        echo CHTML::dibujaEtiqueta("td", array(), CGeneral::fechaMysqlANormal($fila["fecha_fin"]), true);
+						echo CHTML::dibujaEtiqueta("td", array(), $fila["importe"], true);
+						if($fila["anulado"]){
+							echo CHTML::dibujaEtiqueta("td", array(), "ACTIVO", true);
+						}
+						else{
+							echo CHTML::dibujaEtiqueta("td", array(), "ANULADO", true);
+						}
+                        echo CHTML::dibujaEtiqueta("td");
+                            echo CHTML::dibujaEtiqueta("a", array("href"=>"#", "class"=>"btnPagar"));
+                                echo CHTML::dibujaEtiqueta("img", array("src"=>"../../../imagenes/ico_pagar.png"));
+                            echo CHTML::dibujaEtiquetaCierre("a");
+                            echo CHTML::dibujaEtiqueta("a", array("href"=>"#", "class"=>"btnAnular"));
+                                echo CHTML::dibujaEtiqueta("img", array("src"=>"../../../imagenes/ico_borrar.png"));
+                            echo CHTML::dibujaEtiquetaCierre("a");
+                        echo CHTML::dibujaEtiquetaCierre("td");
+                    echo CHTML::dibujaEtiquetaCierre("tr");
+                }            
+            echo CHTML::dibujaEtiquetaCierre("tbody");
+        echo CHTML::dibujaEtiquetaCierre("table");
+    echo CHTML::dibujaEtiquetaCierre("div");
+
+    echo CHTML::script("$('#tActividades').dynatable();");
+   
+   	//VENTANA MODAL PAGAR COMPRA
+	echo CHTML::dibujaEtiqueta("div", array("id"=>"modalPagar", "class"=>"modal fade", "tabindex"=>"-1", "role"=>"dialog"));
+		echo CHTML::dibujaEtiqueta("div", array("class"=>"modal-header"));
+			echo CHTML::boton("x", array("class"=>"close btn", "data-dismiss"=>"modal"));
+			echo CHTML::dibujaEtiqueta("h2", array(), "Confirmar Pago", true);
+		echo CHTML::dibujaEtiquetaCierre("div");
+		echo CHTML::dibujaEtiqueta("div", array("class"=>"modal-body"));
+			echo CHTML::dibujaEtiqueta("p", array(), "¿Pago realizado?", true);
+		echo CHTML::dibujaEtiquetaCierre("div");
+		echo CHTML::dibujaEtiqueta("div", array("class"=>"modal-footer"));
+			echo CHTML::boton("Pagar", array("id"=>"seguroPago", "class"=>"btn"));
+			echo CHTML::boton("Cancelar", array("class"=>"btn", "data-dismiss"=>"modal"));
+		echo CHTML::dibujaEtiquetaCierre("div");
+	echo CHTML::dibujaEtiquetaCierre("div");	
+	
+	//VENTANA MODAL MENSAJE BORRADO
+	echo CHTML::dibujaEtiqueta("div", array("id"=>"modalAnular", "class"=>"modal fade", "tabindex"=>"-1", "role"=>"dialog"));
+		echo CHTML::dibujaEtiqueta("div", array("class"=>"modal-header"));
+			echo CHTML::boton("x", array("class"=>"close btn", "data-dismiss"=>"modal"));
+			echo CHTML::dibujaEtiqueta("h2", array(), "Confirmar Anulación", true);
+		echo CHTML::dibujaEtiquetaCierre("div");
+		echo CHTML::dibujaEtiqueta("div", array("class"=>"modal-body"));
+			echo CHTML::dibujaEtiqueta("p", array(), "¿Desea anular la compra?", true);
+		echo CHTML::dibujaEtiquetaCierre("div");
+		echo CHTML::dibujaEtiqueta("div", array("class"=>"modal-footer"));
+			echo CHTML::boton("Anular", array("id"=>"seguroAnular", "class"=>"btn"));
+			echo CHTML::boton("Cancelar", array("class"=>"btn", "data-dismiss"=>"modal"));
+		echo CHTML::dibujaEtiquetaCierre("div");
+	echo CHTML::dibujaEtiquetaCierre("div");	
