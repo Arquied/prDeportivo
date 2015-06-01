e6fb2b884195f92459d264aeed066839fa7d5240
Arquied <arquied_88@hotmail.com>
2015-06-01 16:58:56 +0200

    Update quien.php

--- aplicacion/vistas/inicial/quien.php
+++ aplicacion/vistas/inicial/quien.php
@@ -1,22 +1,15 @@
 <?php
-
-
 echo CHTML::dibujaEtiqueta("div",array("class"=>"container"));
-
     echo CHTML::dibujaEtiqueta("div", array("class"=>"container contForm"));
-    	echo CHTML::dibujaEtiqueta("div", array("class"=>"contTitFormulario"));
-    	echo CHTML::dibujaEtiqueta("h2", array(), "Quien Somos", true);                
+        echo CHTML::dibujaEtiqueta("div", array("class"=>"contTitFormulario"));
+        echo CHTML::dibujaEtiqueta("h2", array(), "Quien Somos", true);                
    echo CHTML::dibujaEtiquetaCierre("div");
-
 echo CHTML::dibujaEtiqueta("div");
-
-echo CHTML::dibujaEtiqueta("p",array(),$configuracion[0]["descripcion"],true);
-echo CHTML::dibujaEtiqueta("p",array(),"Dirección: ".$configuracion[0]["direccion"],true);
-echo CHTML::dibujaEtiqueta("p",array(),"Telefono: ".$configuracion[0]["telefono"],true);
-echo CHTML::dibujaEtiqueta("p",array(),"Correo electronico: ".$configuracion[0]["correo"],true);
-echo CHTML::dibujaEtiqueta("p", array(),"Url facebook: ".$configuracion[0]["url_facebook"],true);
-echo CHTML::dibujaEtiqueta("p", array(), "Url twitter: ".$configuracion[0]["url_twitter"],true);
-
+echo CHTML::dibujaEtiqueta("p",array(),$configuracion->descripcion,true);
+echo CHTML::dibujaEtiqueta("p",array(),"Dirección: ".$configuracion->direccion,true);
+echo CHTML::dibujaEtiqueta("p",array(),"Telefono: ".$configuracion->telefono,true);
+echo CHTML::dibujaEtiqueta("p",array(),"Correo electronico: ".$configuracion->correo,true);
+echo CHTML::dibujaEtiqueta("p", array(),"Url facebook: ".$configuracion->url_facebook,true);
+echo CHTML::dibujaEtiqueta("p", array(), "Url twitter: ".$configuracion->url_twitter,true);
 echo CHTML::dibujaEtiquetaCierre("div");
-
 echo CHTML::dibujaEtiquetaCierre("div");
