2692fd93651fb374f3971e8695d08619470dac5a
axisco91 <darthxisco@gmail.com>
2015-06-04 17:07:28 +0200

    Create configAuto.php

---
+++ aplicacion/config/configAuto.php
@@ -0,0 +1,11 @@
+<?php
+
+	$config=array("CONTROLADOR"=> array("inicial"),
+				  "RUTAS_INCLUDE"=>array("aplicacion/modelos", "aplicacion/otros"),
+				  "BD"=>array("hay"=>true,
+				              "servidor"=>"localhost",
+							  "usuario"=>"operario",
+							  "contra"=>"oper",
+							  "basedatos"=>"centrodeportivo"),
+				   "SESION"=>array("controlAutomatico"=>false),
+				   "ACCESO"=>array("controlAutomatico"=>false));
