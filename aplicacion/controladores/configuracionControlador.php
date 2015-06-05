44e3ae7a333fc9efe5801c459803ec7b858b102a
Arquied <arquied_88@hotmail.com>
2015-06-05 21:16:35 +0200

    Update configuracionControlador.php

--- aplicacion/controladores/configuracionControlador.php
+++ aplicacion/controladores/configuracionControlador.php
@@ -24,56 +24,54 @@
                         if (!$configuracion -> guardar()) {
                             $this -> dibujaVista("indexConfiguracion", array("modelo" => $configuracion), "Modificar configuración");
                             exit ;
-                        }
-                        if($configuracion->logo!=""){                       
-                            $configuracion -> logo="conf".substr("0000000000".$configuracion->cod_configuracion, -10);
-                            //Cargar imagen
-                            if (isset($_FILES[$configuracion->getNombre()]) && $_FILES[$configuracion->getNombre()]["error"]["imagen"]!=4){
-                                $imagen="";
-                                //segun sea la imagen
-                                switch ($_FILES[$configuracion->getNombre()]["type"]["imagen"]) {
-                                    case 'image/jpeg':
-                                        $imagen=imagecreatefromjpeg($_FILES[$configuracion->getNombre()]["tmp_name"]["imagen"]);
-                                        $configuracion->logo.=".jpg";  
-                                        imagealphablending($imagen, true);
-                                        imagesavealpha($imagen, true);
-                                        if (is_resource($imagen)){
-                                            $ruta="/imagenes/configuracion/".$configuracion->logo;
-                                            $ruta=$_SERVER["DOCUMENT_ROOT"].$ruta;
-                                            imagejpeg($imagen,$ruta);
-                                        }                                   
-                                        break;
-                                        
-                                    case 'image/gif':
-                                        $imagen=imagecreatefromgif($_FILES[$configuracion->getNombre()]["tmp_name"]["imagen"]);
-                                        $configuracion->logo.=".gif";  
-                                        imagealphablending($imagen, true);
-                                        imagesavealpha($imagen, true);
-                                        if (is_resource($imagen)){
-                                            $ruta="/imagenes/configuracion/".$configuracion->logo;
-                                            $ruta=$_SERVER["DOCUMENT_ROOT"].$ruta;
-                                            imagegif($imagen,$ruta);
-                                        }
-                                        break;
-            
-                                    case 'image/png':
-                                        $imagen=imagecreatefrompng($_FILES[$configuracion->getNombre()]["tmp_name"]["imagen"]);
-                                        $configuracion->logo.=".png";  
-                                        imagealphablending($imagen, true);
-                                        imagesavealpha($imagen, true);
-                                        if (is_resource($imagen)){
-                                            $ruta="/imagenes/configuracion/".$configuracion->logo;
-                                            $ruta=$_SERVER["DOCUMENT_ROOT"].$ruta;
-                                            imagepng($imagen,$ruta);
-                                        }
-                                        break;
-                                }
-                            }
-                            if(!$configuracion->guardar()){
-                                $this -> dibujaVista("indexConfiguracion", array("modelo" => $configuracion), "Modificar configuración");
-                                exit ;
+                        }                       
+                        $configuracion -> logo="conf".substr("0000000000".$configuracion->cod_configuracion, -10);
+                        //Cargar imagen
+                        if (isset($_FILES[$configuracion->getNombre()]) && $_FILES[$configuracion->getNombre()]["error"]["imagen"]!=4){
+                            $imagen="";
+                            //segun sea la imagen
+                            switch ($_FILES[$configuracion->getNombre()]["type"]["imagen"]) {
+                                case 'image/jpeg':
+                                    $imagen=imagecreatefromjpeg($_FILES[$configuracion->getNombre()]["tmp_name"]["imagen"]);
+                                    $configuracion->logo.=".jpg";  
+                                    imagealphablending($imagen, true);
+                                    imagesavealpha($imagen, true);
+                                    if (is_resource($imagen)){
+                                        $ruta="/imagenes/configuracion/".$configuracion->logo;
+                                        $ruta=$_SERVER["DOCUMENT_ROOT"].$ruta;
+                                        imagejpeg($imagen,$ruta);
+                                    }                                   
+                                    break;
+                                    
+                                case 'image/gif':
+                                    $imagen=imagecreatefromgif($_FILES[$configuracion->getNombre()]["tmp_name"]["imagen"]);
+                                    $configuracion->logo.=".gif";  
+                                    imagealphablending($imagen, true);
+                                    imagesavealpha($imagen, true);
+                                    if (is_resource($imagen)){
+                                        $ruta="/imagenes/configuracion/".$configuracion->logo;
+                                        $ruta=$_SERVER["DOCUMENT_ROOT"].$ruta;
+                                        imagegif($imagen,$ruta);
+                                    }
+                                    break;
+        
+                                case 'image/png':
+                                    $imagen=imagecreatefrompng($_FILES[$configuracion->getNombre()]["tmp_name"]["imagen"]);
+                                    $configuracion->logo.=".png";  
+                                    imagealphablending($imagen, true);
+                                    imagesavealpha($imagen, true);
+                                    if (is_resource($imagen)){
+                                        $ruta="/imagenes/configuracion/".$configuracion->logo;
+                                        $ruta=$_SERVER["DOCUMENT_ROOT"].$ruta;
+                                        imagepng($imagen,$ruta);
+                                    }
+                                    break;
                             }
                         }
+                        if(!$configuracion->guardar()){
+                            $this -> dibujaVista("indexConfiguracion", array("modelo" => $configuracion), "Modificar configuración");
+                            exit ;
+                        }                        
                         Sistema::app()->irAPagina(array("inicial"));
                         exit ;
                     } 
@@ -88,48 +86,5 @@
             Sistema::app()->paginaError(400, "La configuración no se encuentra");
                 
             } 
-        }
-                        
-           /* if (!Sistema::app() -> acceso() -> hayUsuario()) {
-                  Sistema::app() -> sesion() -> set("pagPrevia", array("configuracion"));
-                  Sistema::app() -> sesion() -> set("parametrosAnt", array("modelo"=>$_POST["configuracion"]));
-                  Sistema::app() -> irAPagina(array("inicial", "login"));
-                  exit ;
-            } 
-            else if (!Sistema::app() -> acceso() -> puedeConfigurar()) {
-                  Sistema::app() -> paginaError(400, "No tiene permiso para acceder");
-                  exit ;
-            } 
-            else {
-                $configuracion = new Configuracion();                 
-                if ($configuracion->buscarPorId(1)){                    
-                    $nombre = $configuracion->getNombre();                    
-                    if (isset($_POST[$nombre])){
-                        
-                        $configuracion->setValores($_POST["nombre"]);
-                        
-                        if ($configuracion->validar()){
-                            
-                            if (!$configuracion->guardar()){
-                              Sistema::app()->paginaError(404,"Se ha producido un error al guardar");
-                              exit;
-                            }
-                            
-                            Sistema::app()->irAPagina(array("configuracion"));
-                            exit;
-                        }
-                        else {
-                            $this->dibujaVista("indexConfiguracion",array("modelo"=>$configuracion),"Modificaci&oacute; de Configuraci$oacuten");
-                            exit;
-                        }
-                        
-                    }
-                    
-                }
-                
-                $this->dibujaVista("indexConfiguracion",array("modelo"=>$configuracion), "Lista de Configuracion");
-                
-            }
-        }*/
-        
+        }       
     }
