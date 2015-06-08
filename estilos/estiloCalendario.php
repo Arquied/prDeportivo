aa20a0c30e5b8c265fef8ec9cd6482be0794ae7a
axisco91 <darthxisco@gmail.com>
2015-06-08 20:25:02 +0200

    Create estiloCalendario.php

---
+++ estilos/estiloCalendario.php
@@ -0,0 +1,19 @@
+.list {
+	width: 20%;
+}
+
+.calendario {
+  width: 100%;
+  max-width: 100%;
+  margin-bottom: 20px;
+}
+
+.calendario > thead > tr > th,
+.calendario > tbody > tr > th,
+.calendario > tfoot > tr > th{
+  padding: 8px;
+  line-height: 1.42857143;
+  border-top: 1px solid #ddd;
+  font-weight: bold;
+  font-size: 2em;
+}
