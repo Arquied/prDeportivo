afba87534e524ff693731d558968495de83d0512
Arquied <arquied_88@hotmail.com>
2015-06-11 12:39:30 +0200

    Update and rename scriptReservasUsuario.js to scriptAnulacionReservas.js

--- script/scriptAnulacionReservas.js
+++ script/scriptAnulacionReservas.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	//EVENTO ONCLICK PARA EL BOTON BORRAR, MUESTRA VENTANA MODAL ESTA SEGURO
 	$(".btnAnular").on("click", function(){
 		$('#modalAnulado').modal({show:true});
-		var id=$(this).parent().parent().prop("id");
+		var id=$(this).prop("id");
 		$("#modalAnulado").data('id_reserva', id);		
 	});
 	//EVENTO ONCLICK CONFIRMAR BORRADO REDIRECCIONA PASANDOLE EL ID_ACTIVIDAD
