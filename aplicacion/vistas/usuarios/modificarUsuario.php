1a3bf9fce0f3f32dbf6db6f551f2cf1d6a3c22fa
axisco91 <darthxisco@gmail.com>
2015-06-15 12:56:03 +0200

    Create modificarUsuario.php

---
+++ aplicacion/vistas/usuarios/modificarUsuario.php
@@ -0,0 +1,104 @@
+<?php
+	//OBTENER ERRORES
+	$errores=$modelo->getErrores();
+	
+	echo CHTML::cssFichero("/estilos/estiloFormularios.css");
+	echo CHTML::scriptFichero("../../script/scriptCrudUsuarios.js");
+
+	echo CHTML::dibujaEtiqueta("div", array("class"=>"container contForm"));
+                echo CHTML::dibujaEtiqueta("div", array("class"=>"contTitFormulario"));
+                    echo CHTML::dibujaEtiqueta("h2", array(), "Modificar usuario", true);                
+                echo CHTML::dibujaEtiquetaCierre("div");
+
+				//FORMULARIO DE REGISTRO
+				echo CHTML::iniciarForm("", "post", array("role"=>"form"));
+				
+					echo CHTML::dibujaEtiqueta("div");
+						//Campo nombre					
+						echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
+							if(isset($errores["nombre"])){
+							echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["nombre"], true);
+							}
+							echo CHTML::modeloLabel($modelo, "nombre");
+							echo CHTML::modeloText($modelo, 
+													"nombre",					
+												 	array("class"=>"form-control", "maxlength"=>80, "size"=>"50"));
+						echo CHTML::dibujaEtiquetaCierre("div");
+
+					echo CHTML::dibujaEtiqueta("div");
+						//Campo correo
+						echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
+							if(isset($errores["correo"])){
+								echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["correo"], true);
+							}
+							echo CHTML::modeloLabel($modelo, "correo");
+							echo CHTML::modeloEmail($modelo, 
+													"correo",										 	 
+												 	array("class"=>"form-control", "maxlength"=>30, "size"=>30));
+						echo CHTML::dibujaEtiquetaCierre("div");
+						
+						//Campo telefono
+						echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
+							if(isset($errores["telefono"])){
+							echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["telefono"], true);
+							}
+							echo CHTML::modeloLabel($modelo, "telefono");
+							echo CHTML::modeloText($modelo,
+													"telefono",
+												 	array("class"=>"form-control", "maxlength"=>9, "size"=>12));
+						echo CHTML::dibujaEtiquetaCierre("div");
+					
+					echo CHTML::dibujaEtiqueta("div");
+						// Campo web o local
+						echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
+						echo CHTML::campoLabel("tipo", "tipo");
+						echo CHTML::dibujaEtiqueta("br");
+						$lista = array("web","Local");
+						echo CHTML::campoListaRadioButton("tipo", "",$lista );
+					echo CHTML::dibujaEtiquetaCierre("div");
+
+					echo CHTML::dibujaEtiqueta("div");
+						//Campo nick					
+						echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
+							if(isset($errores["nick"])){
+								echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["nick"], true);
+							}
+							echo CHTML::modeloLabel($modelo, "nick");
+							echo CHTML::modeloText($modelo, 
+													"nick",
+												 	array("class"=>"form-control", "maxlength"=>30, "size"=>30));
+						echo CHTML::dibujaEtiquetaCierre("div");
+						
+						//Campo contraseña
+						echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
+							if(isset($errores["contrasenia"])){
+							echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["contrasenia"], true);
+							}
+							if(isset($errorCont)){
+							echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errorCont, true);
+							}
+							echo CHTML::modeloLabel($modelo, "contrasenia");
+							echo CHTML::modeloPassword($modelo,
+													"contrasenia",
+												 	array("class"=>"form-control", "maxlength"=>30, "value"=>"", "size"=>30));
+						echo CHTML::dibujaEtiquetaCierre("div");
+						
+						//Campo repite contraseña
+						echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
+							echo CHTML::campoLabel("Repita contraseña", "con1");
+							echo CHTML::campoPassword("con1",
+												 	"", 
+												 	array("class"=>"form-control", "maxlength"=>30, "size"=>30));
+						echo CHTML::dibujaEtiquetaCierre("div");
+					echo CHTML::dibujaEtiquetaCierre("div");
+					
+					echo CHTML::dibujaEtiqueta("div");
+						//Boton insertar
+						echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
+							echo CHTML::campoBotonSubmit("Modificar", array("class"=>"btn btn-default"));					
+						echo CHTML::dibujaEtiquetaCierre("div");
+					echo CHTML::dibujaEtiquetaCierre("div");
+					
+				echo CHTML::finalizarForm();
+				
+	echo CHTML::dibujaEtiquetaCierre("div");
