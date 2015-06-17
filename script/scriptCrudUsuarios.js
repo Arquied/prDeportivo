0bf5454969e8fcff9991a13c13a3e4dc9aeb8e28
Arquied <arquied_88@hotmail.com>
2015-06-17 13:09:58 +0200

    Update scriptCrudUsuarios.js

--- script/scriptCrudUsuarios.js
+++ script/scriptCrudUsuarios.js
@@ -1,22 +1,12 @@
 $(document).ready(function() {	
-	//CARGA DATETIMEPICKER A FECHA DE NACIMIENTO
-	$("#usuario_fecha_nac").datetimepicker({
-				  format:'d/m/Y',
-				  inline:false,
-				  lang:'es',
-				  timepicker:false,
-				  dayOfWeekStart: 1	,
-				  scrollInput: false								  
-				});
-							
 	//EVENTO ONCLICK PARA BOTON CAMBIAR A ADMINISTRADOR, MUESTRA VENTANA MODAL CAMBIAR A ADMINISTRADOR
-	$(".cambiarAdministrador").on("click", function(){
+	$(".table").on("click", ".cambiarAdministrador", function(){
 		var id=$(this).parent().siblings().first().html();
 		$("#modalRole input[name='id_usuario']").val(id);
 		$('#modalRole').modal({show:true});	
 	});
 	//EVENTO ONCLICK PARA EL BOTON BORRAR, MUESTRA VENTANA MODAL ESTA SEGURO
-	$(".btnBorrar").on("click", function(){
+	$(".table").on("click", ".btnBorrar", function(){
 		console.log("pulsado");
 		$('#modalBorrado').modal({show:true});
 		var id=$(this).parent().siblings().first().html();
@@ -26,4 +16,6 @@ $(document).ready(function() {
 	$("#seguroBorrar").on("click", function(){
 		window.location="index.php?co=usuarios&ac=borraUsuario&id="+$("#modalBorrado").data("id_usuario");
 	});
+	
+	
 });
