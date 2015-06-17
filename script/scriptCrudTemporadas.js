acb5a17b13547544109b89919b4b68d08a386d25
Arquied <arquied_88@hotmail.com>
2015-06-17 13:09:23 +0200

    Update scriptCrudTemporadas.js

--- script/scriptCrudTemporadas.js
+++ script/scriptCrudTemporadas.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 	//EVENTO ONCLICK PARA EL BOTON BORRAR, MUESTRA VENTANA MODAL ESTA SEGURO
-	$(".btnBorrar").on("click", function(){
+	$(".table").on("click", ".btnBorrar", function(){
 		$('#modalBorrado').modal({show:true});
-		var id=$(this).parent().siblings().first().html()
+		var id=$(this).parent().siblings().first().html();
 		$("#modalBorrado").data('id_temporada', id);		
 	});
-	//EVENTO ONCLICK CONFIRMAR BORRADO REDIRECCIONA PASANDOLE EL ID_TEMPORADA
+	//EVENTO ONCLICK CONFIRMAR BORRADO REDIRECCIONA PASANDOLE EL ID_INSTALACION
 	$("#seguroBorrar").on("click", function(){
 		window.location="index.php?co=temporadas&ac=borraTemporada&id="+$("#modalBorrado").data("id_temporada");
 	});
