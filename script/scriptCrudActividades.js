1029ae5009e5ec3743061869a1d8f2e61a82df78
Arquied <arquied_88@hotmail.com>
2015-06-17 13:04:12 +0200

    Update scriptCrudActividades.js

--- script/scriptCrudActividades.js
+++ script/scriptCrudActividades.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	//EVENTO ONCLICK PARA EL BOTON BORRAR, MUESTRA VENTANA MODAL ESTA SEGURO
-	$(".btnBorrar").on("click", function(){
+	$(".table").on("click", ".btnBorrar", function(){
 		$('#modalBorrado').modal({show:true});
 		var id=$(this).parent().siblings().first().html()
 		$("#modalBorrado").data('id_actividad', id);		
