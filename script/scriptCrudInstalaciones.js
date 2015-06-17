8f52d175b0df3c9c948b749d5c442520792de39a
Arquied <arquied_88@hotmail.com>
2015-06-17 13:08:59 +0200

    Update scriptCrudInstalaciones.js

--- script/scriptCrudInstalaciones.js
+++ script/scriptCrudInstalaciones.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 	//EVENTO ONCLICK PARA EL BOTON BORRAR, MUESTRA VENTANA MODAL ESTA SEGURO
-	$(".btnBorrar").on("click", function(){
+	$(".table").on("click", ".btnBorrar", function(){
 		$('#modalBorrado').modal({show:true});
-		var id=$(this).parent().siblings().first().html()
+		var id=$(this).parent().siblings().first().html();
 		$("#modalBorrado").data('id_instalacion', id);		
 	});
 	//EVENTO ONCLICK CONFIRMAR BORRADO REDIRECCIONA PASANDOLE EL ID_INSTALACION
