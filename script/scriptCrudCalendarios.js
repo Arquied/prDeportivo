c981cb0889c1f55fccd385d654de9170cc67fed5
axisco91 <darthxisco@gmail.com>
2015-06-18 12:43:46 +0200

    Update scriptCrudCalendarios.js

--- script/scriptCrudCalendarios.js
+++ script/scriptCrudCalendarios.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
 	$(".table").on("click", ".btnBorrar", function(){
 		$('#modalBorrado').modal({show:true});
 		var id=$(this).parent().siblings().first().html();
-		$("#modalBorrado").data('id_instalacion', id);		
+		$("#modalBorrado").data('id_calendario', id);		
 	});
-	//EVENTO ONCLICK CONFIRMAR BORRADO REDIRECCIONA PASANDOLE EL ID_INSTALACION
+	//EVENTO ONCLICK CONFIRMAR BORRADO REDIRECCIONA PASANDOLE EL ID_CALENDARIO
 	$("#seguroBorrar").on("click", function(){
-		window.location="index.php?co=instalaciones&ac=borraInstalacion&id="+$("#modalBorrado").data("id_instalacion");
+		window.location="index.php?co=calendarios&ac=borraCalendario&id="+$("#modalBorrado").data("id_calendario");
 	});
 });
