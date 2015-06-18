dd56c133f870df801b428d5d5d7eb01ea9219951
Arquied <arquied_88@hotmail.com>
2015-06-18 13:33:58 +0200

    Update nuevaTemporada.php

--- aplicacion/vistas/temporadas/nuevaTemporada.php
+++ aplicacion/vistas/temporadas/nuevaTemporada.php
@@ -10,33 +10,30 @@ echo CHTML::dibujaEtiqueta("div", array("class"=>"container contForm"));
 echo CHTML::dibujaEtiqueta("div", array("class"=>"contTitFormulario"));
 echo CHTML::dibujaEtiqueta("h2", array(), "Temporada Nueva", true);                
  echo CHTML::dibujaEtiquetaCierre("div");
+ //CONTENEDOR ERRORES
+echo CHTML::dibujaEtiqueta("div");
+	foreach ($errores as $error) {
+		echo CHTML::dibujaEtiqueta("p", array("class"=>"help-block"), $error, true);	
+	}
+echo CHTML::dibujaEtiquetaCierre("div");
 // FORMULARIO DE NUEVA TEMPORADA
 echo CHTML:: iniciarForm("#","POST",array("role"=>"form"));
 echo CHTML::dibujaEtiqueta("div", array("class"=>"col-sm-offset-3 col-sm-6"));
 // Campo nombre
 echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
-if (isset($errores["nombre"])){
-    echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["nombre"], true);
-}
 echo CHTML::modeloLabel($modelo, "nombre");
 echo CHTML::modeloText($modelo, "nombre", array("maxlength"=>50, "size"=>51, "class"=>"form-control"));
 echo CHTML::dibujaEtiquetaCierre("div");
 // Campo fecha_inicio
 echo CHTML::dibujaEtiqueta("div");
 echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
-if (isset($errores["fecha_inicio"])){
-    echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["fecha_inicio"], true);
-}
 echo CHTML::modeloLabel($modelo, "fecha_inicio");
 echo CHTML::modeloText($modelo, "fecha_inicio", array("class"=>"form-control", "id"=>"fecha_inicio", "maxlength"=>10, "size"=>10, "placeholder"=>"dd/mm/yyyy")); 
 echo CHTML::dibujaEtiquetaCierre("div");
 // Campo fecha_fin
 echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
-if (isset($errores["fecha_fin"])){
-    echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["fecha_fin"], true);
-}
 echo CHTML::modeloLabel($modelo, "fecha_fin");
- echo CHTML::modeloText($modelo, "fecha_fin", array("class"=>"form-control", "id"=>"fecha_fin", "maxlength"=>10, "size"=>10, "placeholder"=>"dd/mm/yyyy")); 
+echo CHTML::modeloText($modelo, "fecha_fin", array("class"=>"form-control", "id"=>"fecha_fin", "maxlength"=>10, "size"=>10, "placeholder"=>"dd/mm/yyyy")); 
 echo CHTML::dibujaEtiquetaCierre("div");
 echo CHTML::dibujaEtiquetaCierre("div");
 // Boton insertar
