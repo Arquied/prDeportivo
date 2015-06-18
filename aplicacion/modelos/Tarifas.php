920358d4253d80c415039a4935bc81644e96506f
axisco91 <darthxisco@gmail.com>
2015-06-18 12:34:15 +0200

    Update Tarifas.php

--- aplicacion/modelos/Tarifas.php
+++ aplicacion/modelos/Tarifas.php
@@ -18,14 +18,16 @@
         
         protected function fijarAtributos() {
             return array("cod_tarifa", "cod_actividad",
-                            "cod_tipo_cuota", "precio", "ocupacion");
+                            "cod_tipo_cuota", "precio", "ocupacion", "disponible");
         }
         protected function fijarDescripciones() {
             return array("cod_tarifa" => "Codigo tarifa:",
                             "cod_actividad"=>"Codigo actividad:",
                             "cod_tipo_cuota"=>"Tipo de cuota:",
                             "precio"=>"Precio:",
-                            "ocupacion"=>"Ocupacion:");
+                            "ocupacion"=>"Ocupacion:",
+                            "disponible"=>"Disponible"
+							);
         }
         protected function fijarRestricciones() {
             Return array(array("ATRI"=>"cod_tarifa", "TIPO"=>"REQUERIDO"),
@@ -36,7 +38,8 @@
                          array("ATRI"=>"cod_tipo_cuota", "TIPO"=>"ENTERO", "MIN"=>0, "MENSAJE"=>"El código de tipo_cuota debe ser positivo", "DEFECTO"=>0),
                          array("ATRI"=>"precio", "TIPO"=>"REQUERIDO"),
                          array("ATRI"=>"precio", "TIPO"=>"REAL", "MIN"=>0.0, "MENSAJE"=>"El precio debe ser positivo", "DEFECTO"=>0.0),
-                         array("ATRI"=>"ocupacion", "TIPO"=>"ENTERO", "MIN"=>0, "MENSAJE"=>"La ocupación debe ser positiva")                     
+                         array("ATRI"=>"ocupacion", "TIPO"=>"ENTERO", "MIN"=>0, "MENSAJE"=>"La ocupación debe ser positiva"),
+						 array("ATRI"=>"disponible", "TIPO"=>"ENTERO", "MAX"=>1, "MIN"=>0)   
                          );
                                 
         }
@@ -46,6 +49,7 @@
             $this -> cod_tipo_cuota = 0;
             $this -> precio = 0.0;
             $this -> ocupacion = 0;
+            $this -> disponible = 1;
         }
         
         protected function fijarSentenciaInsert(){
@@ -53,12 +57,13 @@
             $cod_tipo_cuota = intval($this->cod_tipo_cuota);
             $precio = floatval($this->precio);
             $ocupacion = intval($this->ocupacion);
-            
+            $disponible = intval($this->disponible);
+			
             return "insert into tarifas (".
-                    " cod_actividad, cod_tipo_cuota, precio, ocupacion ".
+                    " cod_actividad, cod_tipo_cuota, precio, ocupacion, disponible ".
                     " ) values ( ".
                     " $cod_actividad, $cod_tipo_cuota, ".
-                    " $precio, $ocupacion ".
+                    " $precio, $ocupacion, $disponible ".
                     " ) " ;
         }
         
@@ -67,12 +72,14 @@
             $cod_tipo_cuota = intval($this->cod_tipo_cuota);
             $precio = floatval($this->precio);
             $ocupacion = intval($this->ocupacion);
+			$disponible = intval($this->disponible);
             
             return "update tarifas set ".
                     " cod_actividad=$cod_actividad, ".
                     " cod_tipo_cuota=$cod_tipo_cuota,".
                     " precio=$precio, ".
-                    " ocupacion=$ocupacion ".
+                    " ocupacion=$ocupacion, ".
+                    " disponible=$disponible ".
                     " where cod_tarifa={$this->cod_tarifa} ";
         }
         
