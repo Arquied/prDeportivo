67063a0c48f093e0bbbb423b077c27ac42bbd240
Arquied <arquied_88@hotmail.com>
2015-06-18 13:30:44 +0200

    Update nuevoPago.php

--- aplicacion/vistas/pagos/nuevoPago.php
+++ aplicacion/vistas/pagos/nuevoPago.php
@@ -8,11 +8,17 @@ $errores = $modelo->getErrores();
     echo CHTML::scriptFichero("../../script/scriptFecha.js");
     echo CHTML::cssFichero("/estilos/estiloFormularios.css");
     
-    
+   
     echo CHTML::dibujaEtiqueta("div", array("class"=>"container contForm"));
         echo CHTML::dibujaEtiqueta("div", array("class"=>"contTitFormulario"));
             echo CHTML::dibujaEtiqueta("h2", array(), "Pagar", true);                
         echo CHTML::dibujaEtiquetaCierre("div");
+		//CONTENEDOR ERRORES
+		echo CHTML::dibujaEtiqueta("div");
+			foreach ($errores as $error) {
+				echo CHTML::dibujaEtiqueta("p", array("class"=>"help-block"), $error, true);	
+			}
+		echo CHTML::dibujaEtiquetaCierre("div");
         // FORMULARIO DE NUEVO PAGO     
         echo CHTML::iniciarForm("", "post", array("role"=>"form", "enctype"=>"multipart/form-data"));
             
@@ -30,9 +36,6 @@ $errores = $modelo->getErrores();
                 // Campo fecha_pago
                 echo CHTML::dibujaEtiqueta("div");
                 echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
-                if(isset($errores["fecha_inicio"])){
-                        echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["fecha_pago"], true);
-                }
                 echo CHTML::modeloLabel($modelo, "fecha_pago");
                 echo CHTML::modeloText($modelo, "fecha_pago", array("class"=>"form-control", "maxlength"=>10, "size"=>10, "placeholder"=>"dd/mm/yyyy"));                        
                 echo CHTML::dibujaEtiquetaCierre("div");  
@@ -42,22 +45,15 @@ $errores = $modelo->getErrores();
             
                 // Campo medio_pago
                 echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
-                    if(isset($errores["cod_medio_pago"])){
-                        echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["cod_medio_pago"], true);
-                    }
                     echo CHTML::campoLabel("MedioPagos", "medioPagos");                            
                     echo CHTML::campoListaDropDown("medioPagos", "", MedioPagos::listaMedioPagos(), array("class"=>"form-control"));
                 echo CHTML::dibujaEtiquetaCierre("div");
             
             echo CHTML::dibujaEtiqueta("div");
                 // Campo importe_pagado
-                echo CHTML::dibujaEtiqueta("div");
                 echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
-                if(isset($errores["importe_pagado"])){
-                        echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["importe_pagado"], true);
-                }
-                echo CHTML::modeloLabel($modelo, "importe_pagado");
-                echo CHTML::modeloText($modelo, "importe_pagado", array("class"=>"form-control", "maxlength"=>10, "size"=>10, "disabled"=>"disabled"));                        
+	                echo CHTML::modeloLabel($modelo, "importe_pagado");
+	                echo CHTML::modeloText($modelo, "importe_pagado", array("class"=>"form-control", "maxlength"=>10, "size"=>10, "disabled"=>"disabled"));                        
                 echo CHTML::dibujaEtiquetaCierre("div"); 
             echo CHTML::dibujaEtiquetaCierre("div");
             
