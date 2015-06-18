55b3601c84dcd468a2500cd07cc1296536248fa5
Arquied <arquied_88@hotmail.com>
2015-06-18 13:32:44 +0200

    Update modificaTarifa.php

--- aplicacion/vistas/tarifas/modificaTarifa.php
+++ aplicacion/vistas/tarifas/modificaTarifa.php
@@ -1,15 +1,18 @@
 <?php
-
 // OBTENER ERRORES
 $errores=$modelo->getErrores();
-
     echo CHTML::cssFichero("/estilos/estiloFormularios.css");
 	
     echo CHTML::dibujaEtiqueta("div", array("class"=>"container contForm"));
                 echo CHTML::dibujaEtiqueta("div", array("class"=>"contTitFormulario"));
                     echo CHTML::dibujaEtiqueta("h2", array(), "Modifica tarifa", true);                
                 echo CHTML::dibujaEtiquetaCierre("div");
-
+				//CONTENEDOR ERRORES
+				echo CHTML::dibujaEtiqueta("div");
+					foreach ($errores as $error) {
+						echo CHTML::dibujaEtiqueta("p", array("class"=>"help-block"), $error, true);	
+					}
+				echo CHTML::dibujaEtiquetaCierre("div");
 				// FORMULARIO DE NUEVA TARIFA
                 echo CHTML::iniciarForm("", "post", array("role"=>"form", "enctype"=>"multipart/form-data"));
                 
@@ -17,18 +20,12 @@ $errores=$modelo->getErrores();
                               
                         //Campo actividad          
                         echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
-                            if(isset($errores["cod_actividad"])){
-                                echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["cod_actividad"], true);
-                            }
                             echo CHTML::modeloLabel($modelo, "cod_actividad");                            
                             echo CHTML::modeloListaDropDown($modelo, "cod_actividad", Actividades::listaActividades(), array("class"=>"form-control"));
                         echo CHTML::dibujaEtiquetaCierre("div");                  
                         
                         // Campo tipo_cuota
                         echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
-							if (isset($errores["cod_tipo_cuota"])){
-								echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["cod_tipo_cuota"], true);
-                            }
 							echo CHTML::modeloLabel($modelo, "cod_tipo_cuota");
 							echo CHTML::modeloListaDropDown($modelo, "cod_tipo_cuota", TiposCuota::listaTiposCuota(),array("class"=>"form-control"));
 					    echo CHTML::dibujaEtiquetaCierre("div");
@@ -36,33 +33,23 @@ $errores=$modelo->getErrores();
                         //Campo precio
                         echo CHTML::dibujaEtiqueta("div");
                         echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
-                            if(isset($errores["precio"])){
-                                echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["precio"], true);
-                            }
-                        echo CHTML::modeloLabel($modelo, "precio");
+                        	echo CHTML::modeloLabel($modelo, "precio");
                             echo CHTML::modeloText($modelo, "precio", array("class"=>"form-control", "id"=>"precio", "maxlength"=>5, "size"=>5));                        
                         echo CHTML::dibujaEtiquetaCierre("div"); 
 						
                         //Campo ocupacion
                         echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
-                            if(isset($errores["ocupacion"])){
-                                echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["ocupacion"], true);
-                            }
-                        echo CHTML::modeloLabel($modelo, "ocupacion");
+                       		echo CHTML::modeloLabel($modelo, "ocupacion");
                             echo CHTML::modeloText($modelo, "ocupacion", array("class"=>"form-control", "id"=>"ocupacion", "maxlength"=>3, "size"=>3));                        
                         echo CHTML::dibujaEtiquetaCierre("div"); 
 						
 						//Campo disponible
                         echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
-                            if(isset($errores["disponible"])){
-                                echo CHTML::dibujaEtiqueta("span", array("class"=>"help-block"), $errores["disponible"], true);
-                            }
                             echo CHTML::modeloLabel($modelo, "disponible");
                             echo CHTML::modeloCheckBox($modelo, "disponible");
                         echo CHTML::dibujaEtiquetaCierre("div");
 						
 						echo CHTML::dibujaEtiquetaCierre("div");
-
                         //Boton insertar
                         echo CHTML::dibujaEtiqueta("div", array("class"=>"form-group"));
                             echo CHTML::campoBotonSubmit("Modifica Tarifa", array("class"=>"btn btn-default"));                 
